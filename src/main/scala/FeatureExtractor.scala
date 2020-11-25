b026aefc7a9a8c0d4b9ad85d4d4702401503846b
ravi0497 <muhammadravi04@gmail.com>
2020-11-25 17:18:43 +0700

    set file

--- src/main/scala/FeatureExtractor.scala
+++ src/main/scala/FeatureExtractor.scala
@@ -32,13 +32,13 @@ class FeatureExtractor(spark:SparkSession,dataframe:DataFrame) {
       expr("tweet.payload.UserMentionEntities.Id") as("Mention_Id"),
       expr("tweet.payload.UserMentionEntities.Name") as("Mention_Name"),
       expr("tweet.payload.UserMentionEntities.Text") as("Mention_UserName")
-    ).filter($"tweet.payload.Lang"==="in")
+    ).filter($"tweet.payload.Lang"==="in" || $"tweet.payload.Lang"==="en")
+
 
   def graphFeature():DataFrame={
     val graphTweet=tweet
       .withColumn("Interactions",
-        getInteraction(col("InReplyToScreenName"),col("Retweet"),
-          col("Mention_UserName")))
+        getInteraction(col("InReplyToScreenName"),col("Retweet"), col("Mention_UserName")))
       .select(
         $"CreatedAt",
         $"Id",
@@ -51,7 +51,8 @@ class FeatureExtractor(spark:SparkSession,dataframe:DataFrame) {
         $"StatusesCount",
         $"Interactions"(0).as("Interaction"),
         $"Interactions"(1).as("Target"),
-        $"Interactions"(2).cast(DataTypes.IntegerType).as("Interaction_weight")
+        $"Interactions"(2).cast(DataTypes.DoubleType).as("Interaction_weight"),
+        $"Interactions"(3).cast(DataTypes.IntegerType).as("Interaction_count")
       )
 
 
@@ -60,11 +61,25 @@ class FeatureExtractor(spark:SparkSession,dataframe:DataFrame) {
         col("Source"),
         col("Target"))
       .agg(
-        sum("Interaction_weight").as("Count_Interaction"),
+        sum("Interaction_count").as("Count_Interaction"),
         avg("Interaction_weight").as("Avg_Interaction")
       )
 
-    weighted_graph.sort($"Count_Interaction".desc)
+    graphTweet.printSchema()
+    weighted_graph.printSchema()
+    weighted_graph
+      .withColumn("Partition",getPartition($"Time.end"))
+      .select(
+        $"Time.start",
+        $"Time.end",
+        $"Source",
+        $"Target",
+        $"Count_Interaction",
+        $"Avg_Interaction",
+        $"Partition"(2).as("Day"),
+        $"Partition"(1).as("Month"),
+        $"Partition"(0).as("Year")
+      )
    }
 
   /**
@@ -79,12 +94,18 @@ class FeatureExtractor(spark:SparkSession,dataframe:DataFrame) {
       val isRetweet=inReplyToScreenName==null && retweet==true
 
     if(isReply){
-      Array("reply",inReplyToScreenName,"3")
+      Array("reply",inReplyToScreenName,"0.75","1")
     }else if(isRetweet){
-      Array("retweet",mention(0),"1")
+      Array("retweet",mention(0),"0.5","1")
     }else{
-      Array("tweet",null,"0")
+      Array("tweet",null,"0.2","1")
     }
   }
 
+  private val getPartition = spark.udf.register("getPartition",partitioning)
+  def partitioning:String=>Array[String]=(column:String)=>{
+      val dateTime=column.split("-")
+
+      Array(dateTime(0),dateTime(1),dateTime(2).split(" ")(0))
+  }
 }
